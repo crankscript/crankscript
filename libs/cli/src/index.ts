73e295dc7152330a9e622532bf4dbb8a10799a9a
Alberto Piu <271597+alberteddu@users.noreply.github.com>
2024-09-16 00:56:18 +0200

    First commit

---
+++ libs/cli/src/index.ts
@@ -0,0 +1,17 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import packageJson from '../package.json';
+
+yargs(hideBin(process.argv))
+    .version(packageJson.version)
+    .scriptName('crankscript')
+    .command(
+        '$0',
+        'says hi',
+        () => {},
+        () => {
+            console.info('Hi! Something here soon.');
+        }
+    )
+    .help()
+    .parse();
