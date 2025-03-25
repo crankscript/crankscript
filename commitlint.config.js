79bd07679c453875633986a6af434dc9d8c04962
Alberto Piu <271597+alberteddu@users.noreply.github.com>
2025-03-25 23:46:12 +0100

    chore(cnk-25): restrict commit format

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    plugins: ['commitlint-plugin-function-rules'],
+    rules: {
+        'scope-empty': [2, 'never'],
+        'subject-case': [
+            2,
+            'never',
+            ['start-case', 'pascal-case', 'upper-case'],
+        ],
+        'function-rules/header-max-length': [
+            2,
+            'always',
+            (parsed) => {
+                if (parsed.scope === 'wip' || /^cnk-\d+$/.test(parsed.scope)) {
+                    return [true];
+                }
+
+                return [false, 'scope must match the pattern "cnk-<xyz>"'];
+            },
+        ],
+    },
+};
