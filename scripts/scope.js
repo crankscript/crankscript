c824504d167e5ccf131621dc7aa41f3e1c6c6214
Alberto <271597+alberteddu@users.noreply.github.com>
2025-03-26 19:25:59 +0100

    chore(cnk-25): restrict commit format (#37)

---
+++ scripts/scope.js
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process';
+
+const getBranchName = () => {
+    try {
+        const branch = execSync('git branch --show-current').toString().trim();
+        const match = branch.match(/cnk-\d+/);
+        return match ? match[0] : undefined;
+    } catch {
+        return undefined;
+    }
+};
+
+console.log(getBranchName());
